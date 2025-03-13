cc2febe3f726f78483373e7ab927c6f314305600
Harsh <harshjindal955@gmail.com>
2025-03-13 21:46:08 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-green-500 text-white rounded-2xl'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
